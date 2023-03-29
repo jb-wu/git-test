332bdd22527663f109a44070f9489f31ea53bbcd
wjb <wjb@163.com>
2023-03-29 10:43:08 +0800

    second commit

---
+++ src/main/java/com/wjb/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.wjb.git;
+
+/**
+ * Author: wjb
+ * Date: 2023-03-29-10:41
+ * Desc:
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git 1");
+        System.out.println("hello git 2");
+    }
+}
